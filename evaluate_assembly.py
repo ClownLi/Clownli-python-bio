064067a1f07caa2589cbdaf6d9281ecfc3983639
limingzhu <42691783+ClownLi@users.noreply.github.com>
2018-09-19 20:44:41 +0800

    Update evaluate_assembly.py

--- evaluate_assembly.py
+++ evaluate_assembly.py
@@ -1,32 +1,34 @@
 '''
-Description : 
-this script is used to evaluate the results of the second
-generation genome sequencing assembly.
-Date :
-2018/09/01
-author:
-lmz 
-'''
+    Description :
+    this script is used to evaluate the results of the second
+    generation genome sequencing assembly.
+    Date :
+    2018/09/01
+    author:
+    lmz
+    '''
 
 import re
 import os
 import argparse
-
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
 
 # Define a list to accept contig sequences
 all_contig = []
 # Define a list to store the length of each contig sequence
 contig_length = []
 
-#Get all contig sequences
 def get_all_seq(file):
-    with open(file,'r') as f:
-        con = f.read()
-        pat = r'(>.*?)\s'
-        reObj = re.compile(pat, re.S)
-        contig_seq = reObj.split(con)
-        for i in range(2,len(contig_seq),2):
-            all_contig.append(''.join(contig_seq[i].split('\n')))
+    for seq_record in SeqIO.parse(file,"fasta"):
+        all_contig.append(str(seq_record.seq))
+
+def count_GC():
+    long_seq = ''.join(all_contig)
+    seq = Seq(long_seq, IUPAC.unambiguous_dna)
+    GC_content = 100 * ((seq.count("G") + seq.count("C")) / len(long_seq))
+    return '%.2f%%' % GC_content
 
 #Get the length of each contig sequence
 def get_length_contig(all_contig):
@@ -66,7 +68,7 @@ def Nxx(percentage):
 
 def main():
     parser = argparse.ArgumentParser(description="Evaluate the results of the second \
-                                                  generation genome sequencing assembly")
+                                     generation genome sequencing assembly")
     parser.add_argument("-i", '--input', help="for example genome.utg.fasta", required=1)
     args = parser.parse_args()
     print(args)
@@ -79,7 +81,8 @@ def main():
     N70 = Nxx(0.7)
     N80 = Nxx(0.8)
     N90 = Nxx(0.9)
-
+    GC = count_GC()
+    print('Percent GC: %s' % GC)                                     
     print('Number of sequence: %d' % len(all_contig))
     print('Min length: %d' % min(contig_length))
     print('Max length: %d' % max(contig_length))
@@ -89,11 +92,10 @@ def main():
     print('N70: %d' % N70[0])
     print('N60: %d' % N60[0])
     print('N50: %d' % N50[0])
-    print('L50: %d' % N50[1])    
+    print('L50: %d' % N50[1])
     print('Average length: %d' % average_length)
     print('Total number (>500bp): %d' % num500)
     print('Total number (>2000bp): %d' % num2000)
 
 if __name__ == "__main__":
     main()
-
